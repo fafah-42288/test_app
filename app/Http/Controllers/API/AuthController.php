c07a655eb2a7a0486a645795946b1a6a5ea29d0c
@DF3232 <danie.faraniaina@relia-consulting.com>
2023-09-25 19:45:59 +0300

    first commit

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{public function createUser(Request $request)
+    {
+        try {
+            //Validated
+            $validateUser = Validator::make($request->all(),
+            [
+                'email' => 'required|email|unique:users,email',
+                'name' => 'required|string|between:2,100',
+                'password' => 'required|string|min:6',
+                'c_password'=>'required|same:password'
+            ]);
+
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Created Successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Login The User
+     * @param Request $request
+     * @return User
+     */
+    public function loginUser(Request $request)
+    {
+        try {
+            $validateUser = Validator::make($request->all(),
+            [
+                'email' => 'required',
+                'password' => 'required'
+            ]);
+
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+            if(!Auth::attempt($request->only(['email', 'password']))){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email & Password does not match with our record.',
+                ], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            return response()->json([
+                'status' => 200,
+                'message' => 'User Logged In Successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+    /**
+ * Logout user (Revoke the token)
+*
+* @return [string] message
+*/
+public function logout(Request $request)
+{ try {
+    $request->user()->tokens()->delete();
+
+    return response()->json([
+
+        'status' => true,
+        'message' => 'Successfully logged out'
+    ],200);
+} catch (\Throwable $th) {
+    return response()->json([
+        'status' => false,
+        'message' => $th->getMessage()
+    ], 500);
+}
+
+
+}
+/**
+ * Get the authenticated User
+*
+* @return [json] user object
+*/
+public function user(Request $request)
+{
+    return response()->json($request->user());
+}
+}
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+      Welcome Home!
+    </div>
+  </template>
+<script setup>
+</script>
